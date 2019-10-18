970824270a65d9552c76ad2720c5c2119828c1f5
JordanG <26260@ma-web.nl>
2019-10-18 14:21:59 +0200

    Fixed with Vector2D

--- html/PerpendicularLine/script.js
+++ html/PerpendicularLine/script.js
@@ -3,18 +3,14 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let a = new Point(390, 200, 20, "rgb(3, 252, 182)");
-let b = new Point(690, 200, 20, "rgb(3, 252, 182)");
-let c = new Point(390, 500, 20, "rgb(3, 252, 182)");
+let a = new Point(new Vector2d(390, 200), 20, "rgb(3, 252, 182)", true);
+let b = new Point(new Vector2d(690, 300), 20, "rgb(3, 252, 182)", true);
+let c = new Point(new Vector2d(390, 500), 20, "rgb(3, 252, 182)", true);
 
-let s = new Point(0, 0, 10, "grey");
+let s = new Point(new Vector2d(0, 0), 10, "grey");
 
 let l = new LinearFunction(0, 0);
 let m = new LinearFunction(0, 0);
-
-a.drag();
-b.drag();
-c.drag();
 
 function animate(){
   requestAnimationFrame(animate);
@@ -24,11 +20,11 @@ function animate(){
   l.draw(context);
 
   m.slope = -1 / l.slope;
-  m.intercept = c.y - m.slope * c.x;
+  m.intercept = c.position.dy - m.slope * c.position.dx;
   m.draw(context);
 
-  s.x = l.intersection(m).x;
-  s.y = l.intersection(m).y
+  s.position.dx = l.intersection(m).x;
+  s.position.dy = l.intersection(m).y;
 
   a.draw(context);
   b.draw(context);
