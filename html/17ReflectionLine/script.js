4bc775708ef32d32b374df3f419f06e3e9e85586
JordanG <26260@ma-web.nl>
2020-03-23 12:18:09 +0100

    ReflectionLine finished

--- html/17ReflectionLine/script.js
+++ html/17ReflectionLine/script.js
@@ -3,13 +3,15 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let a = new Point(new Vector2d(canvas.width / 2 - 100, canvas.height / 2), 20, "blue", true);
+let a = new Point(new Vector2d(canvas.width / 2 - 100, canvas.height / 2 - 10), 20, "blue", true);
 let b = new Point(new Vector2d(canvas.width / 2 + 100, canvas.height / 2), 20, "red", true);
-let s = new Point(new Vector2d(canvas.width / 2 + 100, canvas.height / 2), 20, "white", true);
+let s = new Point(new Vector2d(canvas.width / 2 + 100, canvas.height / 2), 10, "white", false);
 let movingPoint = new DPoint(new Vector2d(canvas.width / 2, canvas.height / 2 + 100), 15, "yellow", new Vector2d(10,10));
 
 let lineA = new LinearFunction(1,1);
 let lineDPoint = new LinearFunction(1,1);
+let rad = new Vector2d(1,1);
+let tan = new Vector2d(1,1);
 
 s.CalcMiddle(a,b);
 
@@ -22,24 +24,38 @@ function animate(){
 
   a.draw(context);
   b.draw(context);
-  s.draw(context);
   movingPoint.update();
   lineDPoint.calcPerp(movingPoint, lineA);
   lineDPoint.draw(context);
   movingPoint.draw(context);
 
-  //tan.dx = -diffAC.dy;
-  //tan.dy = diffAC.dx;
+  let int = {};
+  int = lineDPoint.intersection(lineA);
 
-  //diffAC.magnitude = 1;
-  //tan.magnitude = 1;
+  s.position = new Vector2d(int.x, int.y);
 
-  //diffAC.magnitude = diffAB.dot(diffAC);
-  //tan.magnitude = diffAB.dot(tan);
+  rad.differenceVector(s.position, movingPoint.position);
 
-  //diffAC.draw(context, a.position, diffAC.angle, "yellow", 1);
+  tan.dx = -rad.dy;
+  tan.dy = rad.dx;
 
-  //tan.draw(context, a.position, tan.angle, "yellow", 1);
+  rad.magnitude = 1;
+  tan.magnitude = 1;
+
+  rad.magnitude = movingPoint.vel.dot(rad);
+  tan.magnitude = movingPoint.vel.dot(tan);
+
+  let distance = movingPoint.distanceToAnOtherPoint(s.position);
+
+  if (distance < movingPoint.r) {
+    rad.magnitude = -rad.magnitude;
+    movingPoint.vel.sumVector(rad, tan);
+  }
+
+  s.draw(context);
+  movingPoint.vel.draw(context, movingPoint.position, movingPoint.vel.angle, "yellow", 5);
+  rad.draw(context, s.position, rad.angle, "blue", 5);
+  tan.draw(context, s.position, tan.angle, "green", 5);
 }
 
 animate();
